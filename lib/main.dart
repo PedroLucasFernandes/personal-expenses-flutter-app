8403806a24200970e05008cf48561e1fc6b0e898
Pedro Lucas Fernandes <plff201100@gmail.com>
2023-10-04 12:58:04 -0300

    style: change date format with intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(ExpensesApp());
 
@@ -81,7 +82,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tr.date.toString(),
+                          DateFormat("d MMM y").format(tr.date),
                           style: TextStyle(
                             color: Colors.grey[600],
                           ),
