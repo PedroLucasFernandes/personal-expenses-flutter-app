063d7524037a822ef2c51cde0db21216be4df692
Pedro Lucas Fernandes <plff201100@gmail.com>
2023-10-04 16:42:52 -0300

    feat: Add transaction addition functionality

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -5,6 +5,10 @@ class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
+  final void Function(String, double) onSubmit;
+
+   TransactionForm(this.onSubmit);
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -36,7 +40,9 @@ class TransactionForm extends StatelessWidget {
                     ),
                   ),
                   onPressed: () {
-
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0;
+                    onSubmit(title, value);
                   },
                 ),
               ],
